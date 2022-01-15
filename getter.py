0135dfb283ecf977a88d9d76ad486314ee3eaa6c
mrglaster <50916604+mrglaster@users.noreply.github.com>
2022-01-16 00:27:19 +0800

    Update getter.py

--- getter.py
+++ getter.py
@@ -1,1 +1,18 @@
+import glob
+import os
+import time
+import shutil
+print("Checking existence of Topaz files files")
+path_to_temp = 'Your path to Temp folder (%Temp%)'
+path_to_our_folder = 'Path to destination folder'
+os.chdir(path_to_temp)
+while True:
+    for file in glob.glob("*.xml") + glob.glob("*.bin") + glob.glob("*.onnx"):
+        print(file)
+        try:
+            shutil.copy2(file, path_to_our_folder+file)
+        except:
+            print("Something went wrong with file: "+file)
+    time.sleep(0.5)
+
 
